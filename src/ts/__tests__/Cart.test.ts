4084ed6cff579dcebad659f1994f19c2f310438b
Anton Lysenko <antonlysenko161@yandex.ru>
2025-02-05 20:45:15 +0300

    Реализовал задание и тесты

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,46 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import MusicAlbum from '../domain/MusicAlbum';
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
 });
+
+test('Проверка на добавление элемента', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2500, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  expect(cart.items).toEqual([{ id: 1001, name: 'War and Piece', author: 'Leo Tolstoy', price: 2500, pages: 1225 }, { id: 1008, name: 'Meteora', author: 'Linkin Park', price: 900 }])
+})
+
+test('Проверка на расчет общей стоимости корзины', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2500, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  expect(cart.totalPrice()).toEqual(3400);
+})
+
+test('Проверка на расчет общей стоимости корзины со скидкой', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2500, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  expect(cart.discountTotalPrice(15)).toEqual(2890);
+})
+
+test('Проверка на удаление элемента из корзины по id', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2500, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.deleteItem(1001)
+
+  expect(cart.items).toEqual([{ id: 1008, name: 'Meteora', author: 'Linkin Park', price: 900 }]);
+})
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -3,11 +3,24 @@ import Buyable from '../domain/Buyable';
 export default class Cart {
     private _items: Buyable[] = [];
 
+
     add(item: Buyable): void {
         this._items.push(item);
     }
 
+    totalPrice(): number {
+        return this._items.reduce((sum, item) => sum + item.price, 0);
+    }
+
+    discountTotalPrice(discount: number): number {
+        return this._items.reduce((sum, item) => sum + item.price * (1 - discount * 0.01), 0)
+    }
+
+    deleteItem(id: number): void{
+        this._items = this._items.filter(item => item.id !==id);
+    }
+
     get items(): Buyable[] {
-        return [...this._items]; 
+        return [...this._items];
     }
 }
